84b21f15afe9f4829c789cc3dcda26ce6a2e5e8a
Jureks <uzytkownik@kursuSDA.pl>
2023-10-26 16:49:07 +0200

    Kolejna zmiana

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end("<html><body style="background-color:yellow;"><h1>parasole.gov</h1><br><h3>nr tel. 897 654 321</h3></body></html>");
+    res.end('<html><body style="background-color:yellow;"><h1>parasole.gov</h1><br><h3>nr tel. 897 654 321</h3></body></html>');
 }
 
 const server = http.createServer(requestListener);
